0878914de000c6f4ee869d89f2a8186409d10974
Lincoln Garcia <84988438+lincolngarcia@users.noreply.github.com>
2022-06-14 12:40:24 -0400

    seo beg

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    Welcome to Me.
+    Welcome to Me.<br/>
+
+    Custom, Seamless Websites with every plugin imaginable.<br/><br/>
+
+    seo please let me in I just wanna be seen in google, I'm a developer. I make Websites. Nuxt.js, custom websites. Software Engineering, subway. website design. Programmer. Lincoln Garcia. Lincoln Garcia
   </div>
 </template>
 
@@ -13,6 +17,5 @@ export default {
         description: "Looking for fast, seamless webistes? I'm the Software Engineer for you."
       }
     ]
-  }
-}
+  }}
 </script>
